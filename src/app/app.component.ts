c1a6f748d9cba9afb90d32dbe6f1d32f6185422b
Justin Steines <justins_123@icloud.com>
2021-04-17 16:56:48 -0500

    Additional cleanup of site title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +6,7 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private titleService: Title
-  ) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        const rt = this.getChild(this.activatedRoute);
-        rt.data.subscribe((data) => {
-          this.titleService.setTitle(data.title);
-        });
-      });
-  }
-
-  getChild(activatedRoute: ActivatedRoute): ActivatedRoute {
-    if (activatedRoute.firstChild) {
-      return this.getChild(activatedRoute.firstChild);
-    } else {
-      return activatedRoute;
-    }
-  }
+  ngOnInit(): void {}
 }
